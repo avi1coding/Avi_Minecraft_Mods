d629539a87b7824a9e17ed59f6d9a7fcc0b91ba2
mohd241-svg <mohd241@amityonline.com>
2025-11-05 07:46:00 +0530

    Update UI: Add custom font, green background, footer, and style improvements

--- app.js
+++ app.js
@@ -75,12 +75,16 @@ app.get('/leadership', (req, res) => {
         <!DOCTYPE html>
         <html>
         <head>
+       <link rel="stylesheet" href="style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Minecraft Leaderboard</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
         </head>
-        <body>
+        <body style="background-color: #006400;">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="/">Minecraft Mods</a>
@@ -106,7 +110,7 @@ app.get('/leadership', (req, res) => {
         </div>
     </nav>
         <div class="container mt-5">
-        <h1 class="text-center mb-4">Player Leaderboard</h1>
+        <h1 class="text-center mb-4 text-light audiowide-regular">Player Leaderboard</h1>
         <div class="row">
         `;
         result.forEach(row => {
@@ -140,16 +144,19 @@ app.get('/leadership', (req, res) => {
 </div>
             `;
 
-             page += `
+           
+        });
+        
+         page += `
         </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+            <footer class="bg-dark text-white text-center py-3 mt-5">
+        <p class="mb-0">&copy; 2024 Minecraft Mods Hub</p>
+    </footer>
         </body>
         </html>
         `;
-        });
-        
-       
 
         res.send(page);
 
